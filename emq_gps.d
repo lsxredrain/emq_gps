08185a6ae0361909390ab8779fef83c1ace6627e
lsxredrain <lsxredrain@163.com>
2017-01-11 18:39:15 +0800

    fix bug

--- emq_gps.d
+++ emq_gps.d
@@ -1,7 +1,7 @@
-src/emq_stomp_client.erl:: include/emq_stomp.hrl; @touch $@
-src/emq_stomp_frame.erl:: include/emq_stomp.hrl; @touch $@
-src/emq_stomp_heartbeat.erl:: include/emq_stomp.hrl; @touch $@
-src/emq_stomp_proto.erl:: include/emq_stomp.hrl; @touch $@
-src/emq_stomp_transaction.erl:: include/emq_stomp.hrl; @touch $@
+src/emq_gps_client.erl:: include/emq_gps.hrl; @touch $@
+src/emq_gps_frame.erl:: include/emq_gps.hrl; @touch $@
+src/emq_gps_heartbeat.erl:: include/emq_gps.hrl; @touch $@
+src/emq_gps_proto.erl:: include/emq_gps.hrl; @touch $@
+src/emq_gps_transaction.erl:: include/emq_gps.hrl; @touch $@
 
 COMPILE_FIRST +=
